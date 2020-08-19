c95bebed0fb4f558a7951549f9674959844319bd
MurphyPone <murphyp1@bishopireton.org>
2020-08-19 14:59:58 -0400

    don't update cards until script works

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
+# How to Use
+1. install Python 3.6+
+2. install python packages `pip install -r requirements.txt`
+3. fetch the latest project descriptions with `python fetch.py`
+4. build the website locally to verify everything worked
+5. deploy by pushing to master
+
 # TODOs
 
 - [x] Create skeleton of website
 - [x] Add links to social media
 - [x] Add episode cards
-- [ ] Enable Google analytics
+- [x] Enable Google analytics
     - [ ] Add podcasting email so they can gain insights
-- [ ] Automate fetching the latest descriptions from apple music
-    - [ ] Use selenium to populate a .csv file
+- [x] Automate fetching the latest descriptions from apple music
+    - [ ] fetch streaming links too
 - [ ] Sponsors?
 - [ ] Figure out if all rights _are_ in fact reserved
 - [ ] Containerize it rather than hosting on GHP, may not be necessary depending on how much traffic it gets
 - [ ] Embed Tweets?
+- [ ] Improve build pipeline by adding a developmennt environment rather than solo-doloing inside master
 - [ ] _Maybe_ automate getting the first 30s
+
+
--- js/myscript.js
+++ js/myscript.js
@@ -40,9 +40,40 @@ function typeWriter() {
   }
 }
 
-
-
 /// Automatic tiling thing
 
-var modals = ['portfolioModal0', 'portfolioModal1', 'portfolioModal2', 'portfolioModal3', 'portfolioModal4', 'portfolioModal5'
-]
+var title_ids = ["my-title-0", "my-title-1", "my-title-2", "my-title-3", "my-title-4", "my-title-5"]
+var description_ids = ["my-description-0", "my-description-1", "my-description-2", "my-description-3", "my-description-4", "my-description-5"]
+var summary_ids = ["my-summay-0", "my-summay-1", "my-summay-2", "my-summay-3", "my-summay-4", "my-summay-5"]
+var card_title_ids = ["card-title-0", "card-title-1", "card-title-2", "card-title-3", "card-title-4", "card-title-5",]
+var date_ids = ["my-date-0", "my-date-1", "my-date-2", "my-date-3", "my-date-4", "my-date-5"]
+
+// for(var i = 0; i < descriptions.length; i++) {
+//   var element = document.getElementById(descriptions[i])
+//   console.log(element)
+// }
+
+function doLoad() {
+  typeWriter() // call the typewriter
+  // for(var i = 0; i < title_ids.length; i++) {
+  //   var title_elem = document.getElementById(title_ids[i])
+  //   var descr_elem = document.getElementById(description_ids[i])
+  //   var summary_elem = document.getElementById(summary_ids[i])
+  //   var date_elem = document.getElementById(date_ids[i])
+  //   var card_title_elem = document.getElementById(card_title_ids[i])
+
+  //   // read latest 6 from file
+  //   var title = "title " + i
+  //   var description = "description " + i
+  //   var summary = "summary " + i
+  //   var date = "2020, month, " + i
+
+  //   title_elem.innerHTML = title
+  //   card_title_elem.innerHTML = title
+  //   descr_elem.innerHTML = summary
+  //   summary_elem.innerHTML = "descr" + i
+  //   date_elem.innerHTML = date
+  // }
+  
+  
+}
