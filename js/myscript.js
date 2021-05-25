87eff3cb01a8c2ba3151a801e8f33731b9b2966c
MurphyPOne <MurphyP1@bishopireton.org>
2021-05-25 13:33:45 -0400

    updated myscript

--- js/myscript.js
+++ js/myscript.js
@@ -52,6 +52,8 @@ var list = [
             "\"A podcast where we dissect and analyze the Supreme Court cases that have devastated American civil liberties, like domesticated cats have devastated local bird populations\"", /* Bowles v. Russell */
             "\"A podcast where we dissect and analyze the Supreme Court cases that have swarmed our nation, like a brood of cicadas\"", /* Get Us to v. Seven */
             "\"A podcast where we dissect and analyze the Supreme Court cases that have frayed and flattened our civil rights, like the bristles of an overused toothbrush\"", /* Jones v. Mississippi */
+            "\"A podcast where we dissect and analyze the Supreme Court cases that have sent our nation's values spiraling downward, like Bitcoin after an Elon Musk tweet\"", /* Walmart v. Dukes */
+            
           ]
 
 var txt = list[Math.floor(Math.random() * list.length)]
