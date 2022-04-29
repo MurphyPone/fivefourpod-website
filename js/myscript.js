b8761b19c3f15eb693f39e4c5568967851e07c43
MurphyPOne <MurphyP1@bishopireton.org>
2022-04-28 22:11:38 -0400

    added lassiter

--- js/myscript.js
+++ js/myscript.js
@@ -77,7 +77,8 @@ var list = [
             "\"A podcast where we dissect and analyze the Supreme Court cases that have undermined our democracy, like the CIA is undermining the Russian war effort\"", /* Ginni Mayer */
             "\"A podcast where we dissect and analyze the Supreme Court cases that have sunk the ship of Liberty, like that fire sunk the ship of Audis in the Atlantic Ocean\"", /* Strickland v. Washington */
             "\"A podcast where we dissect and analyze the Supreme Court cases that have stifled our liberty, like allergy season have stuffed our nasal passages\"", /* Lorillard v. Reilly */
-            "\A podcast where we dissect and analyze the Supreme Court cases that have that have left our naiton tired and overburdened, like an accountant during tax season\"", /* INS */
+            "\"A podcast where we dissect and analyze the Supreme Court cases that have left our nation tired and overburdened, like an accountant during tax season\"", /* INS */
+            "\"A podcast where we dissect and analyze the Supreme Court cases that have conducted a hostile take over of our Constitution like Elon Musk is doing to Twitter\"" /* Lassiter v. Department of Social Services */
           ]
 
 var txt = list[Math.floor(Math.random() * list.length)]
